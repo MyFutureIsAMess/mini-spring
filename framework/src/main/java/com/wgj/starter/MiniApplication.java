948768fcc0fbdb1f3e0b9e697d294a02f1eb9513
MyFutureIsAMess <1085844536@qq.com>
2019-07-27 01:07:57 +0800

    inti

---
+++ framework/src/main/java/com/wgj/starter/MiniApplication.java
@@ -0,0 +1,32 @@
+package com.wgj.starter;
+
+/**
+ * @author Guojian Wang
+ * @version 1.0
+ * @date 2019/7/27 - 0:46
+ * @since 1.0
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑, 永无BUG!
+ * 　　　　 ┃　　　┃Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ */
+public class MiniApplication {
+    public static void run(Class<?> cls, String[] args) {
+        System.out.println("Hello mini-spring!");
+    }
+}
---
+++ test/src/main/java/com/wgj/Application.java
@@ -0,0 +1,35 @@
+package com.wgj;
+
+import com.wgj.starter.MiniApplication;
+
+/**
+ * @author Guojian Wang
+ * @version 1.0
+ * @date 2019/7/26 - 23:53
+ * @since 1.0
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑, 永无BUG!
+ * 　　　　 ┃　　　┃Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        MiniApplication.run(Application.class, args);
+    }
+}
